1573f14c636d0e38e25c8995690116db1c465606
Triston Burns <tburns@aurasolutionsllc.com>
2023-05-12 12:51:20 -0700

    Add success case unit test for running a cjs script

--- __test__/exec.test.ts
+++ __test__/exec.test.ts
@@ -26,4 +26,9 @@ describe("exec", () => {
         const executeFileResult = await executeFilePromise('./__test__/testFiles/index.js');
         expect(executeFileResult).toBeTruthy();
     }, 10000);
+
+    it("should successfully execute the CJS script in a JavaScript file", async () => {
+        const executeFileResult = await executeFilePromise('./__test__/testCJSFiles/index.cjs');
+        expect(executeFileResult).toBeTruthy();
+    }, 10000);
 })
---
+++ __test__/testCJSFiles/index.cjs
@@ -0,0 +1,3 @@
+const example = require("./example.cjs");
+
+module.exports = example;
