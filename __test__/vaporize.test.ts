aab8159bce375f8efdd1ab89e092a8d1cb47001e
Triston Burns <tburns@aurasolutionsllc.com>
2023-06-09 16:10:47 -0700

    Update unit tests

--- __test__/vaporize.test.ts
+++ __test__/vaporize.test.ts
@@ -14,12 +14,4 @@ describe('vaporize', () => {
     it('should identify the list of unused dependencies in a JavaScript file', async () => {
         await expect(vaporize(fileURLToPath(pathToFileURL("__test__/testFiles/index.js")))).resolves.toEqual('import example from "./example.js";\r\nexport default example;');
     });
-
-    it('should successfully read & test code outside of vaporize source code', async () => {
-        // use os to create temp test file
-        // name the temp test file
-        // use it to write to test file generated in function
-        // delete the temp test file.
-        await expect(vaporize(fileURLToPath(pathToFileURL("../testRepo/example.js")))).resolves.not.toThrow();
-    });
 });
