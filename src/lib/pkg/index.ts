d1b594dac916b902beb6cd2cc410fc4b6063c4d2
Triston Burns <tristonburns97@gmail.com>
2023-06-24 21:44:45 +0000

    Remove console log

--- src/lib/pkg/index.ts
+++ src/lib/pkg/index.ts
@@ -5,5 +5,4 @@ const packageJson = fs.readFileSync('./package.json', 'utf8');
 
 // Parse package.json
 const { type: moduleType } = JSON.parse(packageJson);
-console.log(moduleType);
 export default moduleType;
