5e2df4b79d666982f70e8f6c848485b9311f1608
Triston Burns <tristonburns97@gmail.com>
2023-07-05 15:50:18 -0700

    Delete test

--- __test__/compile.test.ts
+++
@@ -1,9 +0,0 @@
-import { compileToJavaScriptPromise } from "../src/lib";
-import { randomUUID } from "node:crypto";
-
-describe("compile", () => {
-    it("should successfully compile a TypeScript file to a JavaScript file", async () => {
-        const compileFileResult = await compileToJavaScriptPromise("./__test__/testESMFiles/compile.ts");
-        expect(compileFileResult).toBeTruthy();
-    }, 30000);
-})
