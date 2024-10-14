6145d1a8dec81eb5346f0bf745368a34ea0e8221
Triston Burns <tburns@aurasolutionsllc.com>
2024-10-14 13:14:11 -0700

    Refactor syntax-regex module

--- src/lib/syntax-regex/index.ts
+++ src/lib/syntax-regex/index.ts
@@ -1,34 +1,34 @@
 /**
- * Returns a regex template for commonJS require statement syntax as a raw string.
- * @param {string} dep The name of the dependency.
+ * Returns a regex template for the required commonJS module syntax as a raw string.
+ * @param {string} moduleName The module name.
  * @param {boolean} hasKeyWord A value indicating whether or not the dependency string contains a variable keyword.
  * @returns The regex for the dependency's commonJS require statement.
  */
-export const commonJS = (dep: string, hasKeyWord: boolean = false): string => {
-  return hasKeyWord ? String.raw`(?<=const|let|var)\{*[A-Za-z0-9,]*\}*=require\(["'.\/A-Za-z0-9]*${dep}["']\)` : String.raw`=require\(["'.\/A-Za-z0-9]*${dep}["']\)`;
+export const commonJS = (moduleName: string, hasKeyWord: boolean = false): string => {
+  return hasKeyWord ? String.raw`(?<=const|let|var)\{*[A-Za-z0-9,]*\}*=require\(["'.\/A-Za-z0-9]*${moduleName}["']\)` : String.raw`=require\(["'.\/A-Za-z0-9]*${moduleName}["']\)`;
 };
 
 /**
- * Returns a regex template for ESM imports syntax as a raw string.
- * @param {string} imp The name of the import.
+ * Returns a regex template for the imported ECMAScript module syntax as a raw string.
+ * @param {string} moduleName The module name.
  * @param {boolean} hasImport A value indicating whether or not the import string contains the "import" keyword.
- * @returns the regex for the ESM import.
+ * @returns the regex for the imported ESM module.
  */
-export const esm = (imp: string, hasImport: boolean = false): string => {
-  return hasImport ? String.raw`(?<=import)\{*[A-Za-z0-9,]*\}*from["']${imp}["']` : String.raw`from["']${imp}["']`;
+export const esm = (moduleName: string, hasImport: boolean = false): string => {
+  return hasImport ? String.raw`(?<=import)\{*[A-Za-z0-9,]*\}*from["']${moduleName}["']` : String.raw`from["']${moduleName}["']`;
 };
 
 /**
  * Gets a matching module.
  * @param moduleName The name of the module.
  * @param dependencyString The stringified code for importing a dependency.
- * @param isESM A value indicating whether or not the dependency is an ECMAScript module.
+ * @param isEsm A value indicating whether or not the file uses ESM syntax.
  * @returns The matching module.
  */
-const getMatchingModule = (moduleName: string, dependencyString: string, isESM: boolean = false): string => {
+const getMatchingModule = (moduleName: string, dependencyString: string, isEsm: boolean = false): string => {
     moduleName = moduleName.replace(/\./gm, "\\.");
     moduleName = moduleName.replace(/\//gm, "\\/");
-    const depRegExp: RegExp = new RegExp(isESM ? esm(moduleName, true) : commonJS(moduleName, true), "gm");
+    const depRegExp: RegExp = new RegExp(isEsm ? esm(moduleName, true) : commonJS(moduleName, true), "gm");
     return dependencyString.match(depRegExp)?.[0] || "";
 }
 
@@ -38,9 +38,9 @@ const getMatchingModule = (moduleName: string, dependencyString: string, isESM: 
  * @param dependencyString The stringified code for importing the dependencies.
  * @returns The list of a file's modules.
  */
-export const getModules = (moduleNames: string[], dependencyString: string, isESM: boolean = false): string[] => {
+export const getModules = (moduleNames: string[], dependencyString: string, isEsm: boolean = false): string[] => {
     return moduleNames.map((imp: string) => {
-        const match = getMatchingModule(imp, dependencyString, isESM);
+        const match = getMatchingModule(imp, dependencyString, isEsm);
         if (match) {
             dependencyString = dependencyString.replace(new RegExp(String.raw`["']${imp}["']`), "");
             return match;
@@ -48,10 +48,17 @@ export const getModules = (moduleNames: string[], dependencyString: string, isES
     }).filter(x => x);
 }
 
-export const getVariableNames = (requirements: string[], dependencies: string[], isModuleType: boolean = false): string[] => {
+/**
+ * Gets the list of the variable names used to store modules.
+ * @param requirements The list of a file's requirements.
+ * @param modules The list of modules.
+ * @param isEsm A value indicating whether or not the file uses ESM syntax.
+ * @returns The list of module variable names.
+ */
+export const getVariableNames = (requirements: string[], modules: string[], isEsm: boolean = false): string[] => {
     const variableNames: string[] = [];
     for (let i = 0; i < requirements.length; i++) {
-        let variableName = requirements[i].replace(new RegExp(isModuleType ? esm(dependencies[i]) : commonJS(dependencies[i]), "gm"), "");
+        let variableName = requirements[i].replace(new RegExp(isEsm ? esm(modules[i]) : commonJS(modules[i]), "gm"), "");
         if (variableName.includes("{")) {
             variableName = variableName.replace("{", "");
         }
@@ -72,6 +79,12 @@ export const getVariableNames = (requirements: string[], dependencies: string[],
     return variableNames;
 }
 
+/**
+ * Finds matching variable references within a file.
+ * @param variableName The name of the variable.
+ * @param fileString The stringified file code.
+ * @param unusedReferences The list of unused references.
+ */
 export const findVariableReferences = (variableName: string, fileString: string, unusedReferences: string[]): void => {
     if (!fileString.match(new RegExp(String.raw`(?<!"|'|\`|\/|\.)\b${variableName}\b(?!"|'|\`|\/)`, "gm"))?.length) {
         unusedReferences.push(variableName);
